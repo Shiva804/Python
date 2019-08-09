54998f78585a5104b72e569f6e223c9b186664a0
Shiva804 <krishnansai49@gmail.com>
2019-08-09 18:52:36 +0530

    Update CheckList1.py

--- CheckList1.py
+++ CheckList1.py
@@ -12,9 +12,9 @@ list2=ph.readcomp()
 
 layout=[
     [sh.Text('Enter TO DO list:',text_color="blue",background_color="cyan")],
-    [sh.InputText('',key='name',background_color="Sky Blue"),sh.Button('ADD',key="add",size=(6,1)),sh.CalendarButton("SELECT DATE",target="date",key="dat"),sh.InputText("",key = "date", disabled=True ,do_not_clear=False)],
+    [sh.InputText('',key='name',background_color="Sky Blue"),sh.Button('ADD',key="add",size=(6,1)),sh.CalendarButton("SELECT DATE",target="date",key="dat"),sh.InputText("",key = "date",size=(19,1),disabled=True ,do_not_clear=True),sh.Text("Priority:",text_color="blue",background_color="cyan"),sh.Spin([sz for sz in range(1, 6)], initial_value=1, change_submits=True, key='spin',size=(5,3),background_color="Sky Blue")],
     [sh.Text("All Tasks",text_color="blue",background_color="cyan"),sh.Listbox(size=(30,10),key="box", enable_events= True ,background_color="Sky Blue",values=list),sh.Text("Completed Tasks",text_color="blue",background_color="cyan"),sh.Listbox( enable_events= True,size=(30,10),key="cbox",background_color="Sky Blue",values=list2),sh.Button("Delete Completed",size=(13,1),key="delcomp")],
-    [sh.Button("DELETE",key="del"),sh.Button("Completed",size=(8,1),key="comp",)],[sh.Text("Priority:",text_color="blue",background_color="cyan"),sh.Spin([sz for sz in range(1, 6)], initial_value=1, change_submits=True, key='spin',size=(5,3),background_color="Sky Blue"),sh.Button("prioritize")],
+    [sh.Button("DELETE",key="del"),sh.Button("Completed",size=(8,1),key="comp",),sh.Button("Prioritize")],
     [sh.Button("EXIT",size=(5,1))]
 
 ]
@@ -23,7 +23,9 @@ form = sh.FlexForm('Calendar', no_titlebar=True)
 
 window=sh.Window("CheckBox",layout,background_color="cyan")
 if len(list)==0:
-    sh.Popup("You don't have any tasks")
+    sh.Popup("You don't have any tasks!")
+if len(list)>10:
+    sh.Popup("You have lots of work to do!")
 
 while True:
     event,values=window.Read()
@@ -41,8 +43,10 @@ while True:
         list.append(x)
         window.FindElement('box').Update(values=list)
         window.FindElement("name").Update("")
-        window.FindElement('dat').Update("SELECT DATE")
+        #window.FindElement('dat').Update("SELECT DATE")
+        window.FindElement('date').Update("")
         window.FindElement('add').Update("ADD")
+        
         ph.add(list)
 
       
@@ -68,7 +72,7 @@ while True:
         window.FindElement('comp').Update("Completed")
         
 
-    elif( event == "prioritize"):
+    elif( event == "Prioritize"):
          for i in range(len(list)):
              min = i
              for j in range(i+1, len(list)):
